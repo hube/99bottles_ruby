47387d6f8f51ef55834e79f2db05c9165a5782bb
Hubert Lee <hube@users.noreply.github.com>
2020-10-02 20:03:18 -0700

    Support verse 0

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -7,9 +7,14 @@ class Bottles
       "1 bottle of beer.\n" +
       "Take it down and pass it around, " +
       "no more bottles of beer on the wall.\n"
+  ZEROTH_VERSE = "No more bottles of beer on the wall, " +
+      "no more bottles of beer.\n" +
+      "Go to the store and buy some more, " +
+      "99 bottles of beer on the wall.\n"
 
   def verse(verse_num)
     return FINAL_VERSE if verse_num == 1
+    return ZEROTH_VERSE if verse_num == 0
 
     last_bottle = verse_num == 2 ? "bottle" : "bottles"
     return VERSE % [verse_num, verse_num, verse_num - 1, last_bottle]

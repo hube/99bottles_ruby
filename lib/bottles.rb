2ca18d7a8469fc3aa8646cd7af2771b8b7e3a507
Hubert Lee <hube@users.noreply.github.com>
2020-10-02 20:02:36 -0700

    Support verse 1

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -3,8 +3,14 @@ class Bottles
       "%s bottles of beer.\n" +
       "Take one down and pass it around, " +
       "%s %s of beer on the wall.\n"
+  FINAL_VERSE = "1 bottle of beer on the wall, " +
+      "1 bottle of beer.\n" +
+      "Take it down and pass it around, " +
+      "no more bottles of beer on the wall.\n"
 
   def verse(verse_num)
+    return FINAL_VERSE if verse_num == 1
+
     last_bottle = verse_num == 2 ? "bottle" : "bottles"
     return VERSE % [verse_num, verse_num, verse_num - 1, last_bottle]
   end

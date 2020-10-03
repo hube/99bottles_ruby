614081ad7d07c414c059d7ceef13664d13e326b2
Hubert Lee <hube@users.noreply.github.com>
2020-10-02 19:55:29 -0700

    Support another verse

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -3,9 +3,16 @@ class Bottles
   end
 
   def verse(verse_num)
-    "99 bottles of beer on the wall, " +
-    "99 bottles of beer.\n" +
-    "Take one down and pass it around, " +
-    "98 bottles of beer on the wall.\n"
+    if verse_num == 99
+      "99 bottles of beer on the wall, " +
+      "99 bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "98 bottles of beer on the wall.\n"
+    elsif verse_num == 3
+      "3 bottles of beer on the wall, " +
+      "3 bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "2 bottles of beer on the wall.\n"
+    end
   end
 end

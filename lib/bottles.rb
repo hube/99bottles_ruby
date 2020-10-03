fcab96650b435ffa4a2bca3a2a453590a2f282bd
Hubert Lee <hube@users.noreply.github.com>
2020-10-02 20:02:20 -0700

    Support verse 2

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,18 +1,11 @@
 class Bottles
-  def initialize
-  end
+  VERSE = "%s bottles of beer on the wall, " +
+      "%s bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "%s %s of beer on the wall.\n"
 
   def verse(verse_num)
-    if verse_num == 99
-      "99 bottles of beer on the wall, " +
-      "99 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "98 bottles of beer on the wall.\n"
-    elsif verse_num == 3
-      "3 bottles of beer on the wall, " +
-      "3 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "2 bottles of beer on the wall.\n"
-    end
+    last_bottle = verse_num == 2 ? "bottle" : "bottles"
+    return VERSE % [verse_num, verse_num, verse_num - 1, last_bottle]
   end
 end

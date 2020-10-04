1ee65e1d2eedbb2b4f7e3fcbe1c479a0dda83c36
Hubert Lee <hube@users.noreply.github.com>
2020-10-03 23:45:55 -0700

    Move verse 1 closer to default

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,16 +1,16 @@
 class Bottles
   def verse(verse_num)
     case verse_num
-    when 1
-      "1 bottle of beer on the wall, " +
-      "1 bottle of beer.\n" +
-      "Take it down and pass it around, " +
-      "no more bottles of beer on the wall.\n"
     when 0
       "No more bottles of beer on the wall, " +
       "no more bottles of beer.\n" +
       "Go to the store and buy some more, " +
       "99 bottles of beer on the wall.\n"
+    when 1
+      "1 bottle of beer on the wall, " +
+      "1 bottle of beer.\n" +
+      "Take it down and pass it around, " +
+      "no more bottles of beer on the wall.\n"
     else
       "#{verse_num} #{container(verse_num)} of beer on the wall, " +
       "#{verse_num} #{container(verse_num)} of beer.\n" +

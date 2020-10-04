ff0fc2f2be93ab6e42e5b51e7f27dcc82cba1e63
Hubert Lee <hube@users.noreply.github.com>
2020-10-04 00:48:57 -0700

    Eliminate case in verse method

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,17 +1,9 @@
 class Bottles
   def verse(verse_num)
-    case verse_num
-    when 0
-      "No more bottles of beer on the wall, " +
-      "no more bottles of beer.\n" +
-      "Go to the store and buy some more, " +
-      "99 bottles of beer on the wall.\n"
-    else
-      "#{verse_num} #{container(verse_num)} of beer on the wall, " +
-      "#{verse_num} #{container(verse_num)} of beer.\n" +
-      "Take #{pronoun(verse_num)} down and pass it around, " +
-      "#{quantity(verse_num - 1)} #{container(verse_num - 1)} of beer on the wall.\n"
-    end
+    "#{quantity(verse_num).capitalize} #{container(verse_num)} of beer on the wall, " +
+    "#{quantity(verse_num)} #{container(verse_num)} of beer.\n" +
+    "#{action(verse_num)}, " +
+    "#{quantity(successor(verse_num))} #{container(verse_num - 1)} of beer on the wall.\n"
   end
 
   def verses(starting_verse, ending_verse)
@@ -47,7 +39,23 @@ class Bottles
     if count == 0
       "no more"
     else
-      count
+      count.to_s
+    end
+  end
+
+  def action(count)
+    if count == 0
+      "Go to the store and buy some more"
+    else
+      "Take #{pronoun(count)} down and pass it around"
+    end
+  end
+
+  def successor(count)
+    if count == 0
+      99
+    else
+      count - 1
     end
   end
 end

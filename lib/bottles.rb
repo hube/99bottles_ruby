c05559e2559da0a801348b9796a5a199f0060d2b
Hubert Lee <hube@users.noreply.github.com>
2020-10-03 22:18:18 -0700

    Use case statement for verses

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -2,22 +2,31 @@ class Bottles
   VERSE = "%s bottles of beer on the wall, " +
       "%s bottles of beer.\n" +
       "Take one down and pass it around, " +
-      "%s %s of beer on the wall.\n"
-  FINAL_VERSE = "1 bottle of beer on the wall, " +
+      "%s bottles of beer on the wall.\n"
+  VERSE_2 = "2 bottles of beer on the wall, " +
+      "2 bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "1 bottle of beer on the wall.\n"
+  VERSE_1 = "1 bottle of beer on the wall, " +
       "1 bottle of beer.\n" +
       "Take it down and pass it around, " +
       "no more bottles of beer on the wall.\n"
-  ZEROTH_VERSE = "No more bottles of beer on the wall, " +
+  VERSE_0 = "No more bottles of beer on the wall, " +
       "no more bottles of beer.\n" +
       "Go to the store and buy some more, " +
       "99 bottles of beer on the wall.\n"
 
   def verse(verse_num)
-    return FINAL_VERSE if verse_num == 1
-    return ZEROTH_VERSE if verse_num == 0
-
-    last_bottle = verse_num == 2 ? "bottle" : "bottles"
-    return VERSE % [verse_num, verse_num, verse_num - 1, last_bottle]
+    case verse_num
+    when 2
+      VERSE_2
+    when 1
+      VERSE_1
+    when 0
+      VERSE_0
+    else
+      VERSE % [verse_num, verse_num, verse_num - 1]
+    end
   end
 
   def verses(starting_verse, ending_verse)
